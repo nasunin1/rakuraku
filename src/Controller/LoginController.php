61ee4619cca5faaacc4d9ea0008b88db9b49ce9a
nasunin1 <oic.a.tokuyama@gmail.com>
2017-11-06 11:34:45 +0900

    終わりの始まり

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class LoginController extends AppController
+{
+    public function index()
+    {
+
+    }
+}
---
+++ src/Template/Login/index.ctp
@@ -0,0 +1,1 @@
+<button class="btn-danger">hello world</button>
